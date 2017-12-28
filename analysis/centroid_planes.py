880806f1880b389b1b7e9fc2508c274383cea6c8
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-12-28 13:29:29 -0700

    drop some extreme velocities from sio

--- analysis/centroid_planes.py
+++ analysis/centroid_planes.py
@@ -170,12 +170,12 @@ for linename,(vmin,vmax),limits in (('Unknown_4', (-15, 27), (-0.1, 0.1, -0.12, 
     assumed_vcen = 6*u.km/u.s
     show_keplercurves(ax2, 0*u.deg, 150, assumed_vcen, yaxis_unit=u.km/u.s, radii={})
 
-    xx_thindisk, yy_thindisk = thindiskcurve(mass=20*u.M_sun, rmin=20*u.au, rmax=50*u.au)
+    xx_thindisk, yy_thindisk = thindiskcurve(mass=20*u.M_sun, rmin=30*u.au, rmax=80*u.au)
     ax2.plot((xx_thindisk / d_orion).to(u.arcsec, u.dimensionless_angles()),
              yy_thindisk + assumed_vcen,
              'k:',
              transform=trans)
-    xx_thindisk, yy_thindisk = thindiskcurve(mass=5*u.M_sun, rmin=20*u.au, rmax=50*u.au)
+    xx_thindisk, yy_thindisk = thindiskcurve(mass=10*u.M_sun, rmin=20*u.au, rmax=50*u.au)
     ax2.plot((xx_thindisk / d_orion).to(u.arcsec, u.dimensionless_angles()),
              yy_thindisk + assumed_vcen,
              'k-',
