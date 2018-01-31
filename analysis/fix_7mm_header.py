a81c1bc4ec309ffa3216bf67b89098c9ecba144f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-01-31 14:27:30 -0700

    add a fix-7mm-header script to manually match it to existing data

---
+++ analysis/fix_7mm_header.py
@@ -0,0 +1,12 @@
+from astropy.io import fits
+import paths
+
+
+fh = fits.open(paths.dpath('srci_reid.7mm.fits'))
+fh[0].header['CRPIX1'] = 111.954
+fh[0].header['CRPIX2'] = 124.414
+fh[0].header['CRVAL1'] = 83.81049
+fh[0].header['CRVAL2'] = -5.3751712
+fh[0].header['RADESYS'] = 'ICRS'
+
+fh.writeto(paths.dpath('srci_reid.7mm.fixed.fits'), overwrite=True)
