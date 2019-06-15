059d9a8c6928b9404b9821200c630c0c14e842ea
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-06-15 18:22:52 +0300

    create new imaging scripts

---
+++ reduction/line_to_image_list.py
@@ -0,0 +1,67 @@
+from astropy import units as u
+ms_basepath = '/export/home/rng90003/orion/2016.1.00165.S/measurement_sets/'
+
+
+mses = {'b3': ['member.uid___A001_X88e_X1d9_calibrated.ms'],
+        'b6': ['uid___A001_X88e_X1d3_calibrated.ms',
+               'uid___A002_Xb925ef_X4334_calibrated.ms'],
+        'b7': ['band7.ms', 'band7_lb.ms'],
+       }
+
+imaging_parameters = {
+    'b3': {'imsize': [4800, 4800],
+           'cell': ['0.016arcsec'],
+           'threshold': '1mJy',
+           'niter': 1000,
+          },
+    'b6':{'imsize': [7168, 7168],
+          'cell': ['0.004arcsec'],
+          'threshold': '10mJy',
+          'niter': 1000,
+         },
+    'b7':{'imsize': [6720, 6720],
+          'cell': ['0.004arcsec'],
+          'threshold': '15mJy',
+          'niter': 1000,
+         }
+}
+
+
+line_to_image_list = [
+    {'band': 6, 'name': 'H2Ov2=1_5(5,0)-6(4,3)', 'frequency': 232.6867*u.GHz, 'velocity_range': (-30, 40)},
+    {'name': 'SiS_12-11', 'frequency': 217.817644*u.GHz, 'band': 6, },
+    {'name': '29SiOv=0_5-4', 'frequency': 214.3857577*u.GHz,'band': 6, },
+    {'name': '29SiOv=0_2-1', 'frequency': 85.759199*u.GHz,'band': 3, },
+    {'name': '29SiOv=2_2-1', 'frequency': 85.640452*u.GHz,'band': 3, },
+    {'name': 'SiOv=1_2-1', 'frequency': 86.24343*u.GHz,'band': 3, },
+    {'name': 'SiOv=0_2-1', 'frequency': 86.84696*u.GHz,'band': 3, },
+    {'name': 'SiOv=0_5-4', 'frequency': 217.10498*u.GHz,'band': 6, },
+    {'name': 'SiOv=1_5-4', 'frequency': 215.59595*u.GHz,'band': 6, },
+    {'name': 'H30a', 'frequency': 231.900928*u.GHz,'band': 6, },
+    {'name': 'SO2_2-1_1', 'frequency': 86.09395*u.GHz,'band': 3, },
+    {'name': 'SO3_2-2_1', 'frequency': 99.299905*u.GHz,'band': 3, },
+    {'name': 'SOv=1_3_2-2_1', 'frequency': 98.758192*u.GHz,'band': 3, },
+    {'name': 'SO8_8-7_7', 'frequency': 344.310612*u.GHz,'band': 7, },
+    {'name': 'SO9_8-8_7', 'frequency': 346.528481*u.GHz,'band': 7, },
+    {'name': '29SiOv=0_8-7', 'frequency': 342.9808425*u.GHz,'band': 7, },
+    {'name': '29SiOv=1_8-7', 'frequency': 340.6118623*u.GHz,'band': 7, },
+    {'name': '29SiOv=2_8-7', 'frequency': 338.2451561*u.GHz,'band': 7, },
+    {'name': 'SiOv=0_8-7', 'frequency': 347.330824*u.GHz,'band': 7, },
+    {'name': 'SiOv=1_8-7', 'frequency': 344.916247*u.GHz,'band': 7, },
+    {'name': 'SiOv=2_8-7', 'frequency': 342.504607*u.GHz,'band': 7, },
+    {'name': '30SiOv=0_8-7', 'frequency': 338.9300437*u.GHz,'band': 7, },
+    {'name': '30SiOv=1_8-7', 'frequency': 336.6029763*u.GHz,'band': 7, },
+    {'name': '30SiOv=2_8-7', 'frequency': 334.2781299*u.GHz,'band': 7, },
+    {'name': 'SiS19-18', 'frequency': 344.779481*u.GHz,'band': 7, },
+    {'name': 'H13CN1-0', 'frequency': 86.3401764*u.GHz,'band': 3, },
+    {'name': 'HC15N1-0', 'frequency': 86.0549664*u.GHz,'band': 3, },
+    {'name': 'HCN1-0', 'frequency': 88.6318473*u.GHz,'band': 3, },
+    {'name': 'HC3N11-10', 'frequency': 100.076386*u.GHz,'band': 3, },
+    {'name': 'HC3Nv7=1_11-10', 'frequency': 100.3224109*u.GHz,'band': 3, },
+    {'name': 'CS2-1', 'frequency': 97.980953*u.GHz,'band': 3, },
+    {'name': 'SO4_5-4_4', 'frequency': 100.029565*u.GHz,'band': 3, },
+    {'name': 'SOv=1_4_5-4_4', 'frequency': 98.317606*u.GHz,'band': 3, },
+    ]
+for entry in line_to_image_list:
+    if 'velocity_range' not in entry:
+        entry['velocity_range'] = (-30, 40)
---
+++ reduction/scriptForImaging_selected_lines.py
@@ -0,0 +1,79 @@
+"""
+This is a CASA imaging script, so it should be run from within casa with
+%run -i or similar.  Therefore, CASA tasks are not imported.
+"""
+import numpy as np
+import os
+import glob
+import datetime
+from astropy import constants
+from astropy import units as u
+
+from line_to_image_list import mses, line_to_image_list, imaging_parameters, ms_basepath
+
+def makefits(myimagebase, cleanup=True):
+    impbcor(imagename=myimagebase+'.image', pbimage=myimagebase+'.pb', outfile=myimagebase+'.image.pbcor', overwrite=True) # perform PBcorr
+    exportfits(imagename=myimagebase+'.image.pbcor', fitsimage=myimagebase+'.image.pbcor.fits', dropdeg=True, overwrite=True) # export the corrected image
+    exportfits(imagename=myimagebase+'.pb', fitsimage=myimagebase+'.pb.fits', dropdeg=True, overwrite=True) # export the PB image
+    exportfits(imagename=myimagebase+'.model', fitsimage=myimagebase+'.model.fits', dropdeg=True, overwrite=True) # export the PB image
+    exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', dropdeg=True, overwrite=True) # export the PB image
+
+    if cleanup:
+        for suffix in ('psf', 'weight', 'sumwt', 'pb', 'model', 'residual',
+                       'mask', 'image', 'workdirectory'):
+            os.system('rm -rf {0}.{1}'.format(myimagebase, suffix))
+
+
+for line_info in line_to_image_list:
+
+    v0 = u.Quantity(line_info['velocity_range'][0], u.km/u.s)
+    v1 = u.Quantity(line_info['velocity_range'][1], u.km/u.s)
+    band = 'b{0}'.format(line_info['band'])
+    linename = line_info['name']
+    frequency = line_info['frequency'].to(u.Hz).value
+
+    msfile = os.path.join(ms_basepath,
+                          mses[band])
+
+    msmd.open(msfile)
+    spws = msmd.spwsforfield('Orion_BNKL_source_I')
+    spws = [x for x in spws
+            if (msmd.chanfreqs(x).min() < frequency) and
+            (msmd.chanfreqs(x).max() > frequency)]
+    dnu = np.diff(msmd.chanfreqs(spws[0])).mean()
+    dv = (dnu / frequency * constants.c).to(u.km/u.s)
+    nchan = int(np.abs(((v1-v0)/dv).decompose()))
+    msmd.close()
+
+    spw = ",".join([str(spw) for spw in spws])
+
+    start_vel = v0 if dv > 0*u.km/u.s else v1
+
+    for suffix, niter in (('clarkclean1000', 1000), ):
+
+
+        imagename = 'OrionSourceI.{3}.spw{0}.{2}.{1}'.format(spw, suffix, linename, band)
+        if not os.path.exists("{0}.image.pbcor.fits".format(imagename)):
+            print("Imaging {0} at {1}".format(imagename, datetime.datetime.now()))
+            tclean(vis=msfile,
+                   imagename=imagename,
+                   field='Orion_BNKL_source_I',
+                   spw=spw,
+                   gridder='standard',
+                   specmode='cube',
+                   start='{0}km/s'.format(start_vel.to(u.km/u.s).value),
+                   nchan=nchan,
+                   restfreq=frequency,
+                   veltype='radio',
+                   outframe='LSRK',
+                   interactive=F,
+                   niter=imaging_parameters[band]['niter'],
+                   imsize=imaging_parameters[band]['imsize'],
+                   cell=imaging_parameters[band]['cell'],
+                   weighting='briggs',
+                   robust=0.5,
+                   phasecenter='',
+                   threshold=imaging_parameters[band]['threshold'],
+                   savemodel='none',
+                  )
+            makefits(imagename)
