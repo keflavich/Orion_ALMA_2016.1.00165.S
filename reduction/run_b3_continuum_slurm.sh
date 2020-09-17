bbd73e5632425c5d28758a3fe70ce027caeb4d52
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-09-17 10:59:17 -0400

    add bibmem run for b3

---
+++ reduction/run_b3_continuum_slurm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --mail-type=NONE          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=adamginsburg@ufl.edu     # Where to send mail	
+#SBATCH --ntasks=8
+#SBATCH --mem=256gb                     # Job memory request
+#SBATCH --partition=bigmem
+#SBATCH --nodes=1
+#SBATCH --time=96:00:00               # Time limit hrs:min:sec
+#SBATCH --qos=adamginsburg-b
+#SBATCH --account=adamginsburg
+pwd; hostname; date
+
+module load git
+
+which python
+which git
+
+git --version
+echo $?
+
+cd /orange/adamginsburg/orion/2016.1.00165.S/imaging
+
+export CASA=/blue/adamginsburg/adamginsburg/casa/casa-release-5.7.0-134.el7/bin/casa
+#export CASA=/orange/adamginsburg/casa/casa-release-5.6.0-60.el7/bin/casa
+
+xvfb-run -d ${CASA}  --nogui --nologger -c "execfile('continuum_imaging_b3_sep17_2020.py')"
